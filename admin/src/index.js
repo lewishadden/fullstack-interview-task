00071a3dadf581cdba7770a2334225981a2220cf
Lewis Hadden <lewishadden@gmail.com>
2024-03-11 14:26:25 +0000

    Use ESM for admin service

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,28 +1,29 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const config = require("config")
-const request = require("request")
+import express from "express";
+import config from "config";
+import { inspect } from "util";
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(express.json({ limit: "10mb" }));
 
-app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
-})
+app.get("/investments/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const resp = await fetch(
+      `${config.investmentsServiceUrl}/investments/${id}`
+    );
+    const investments = await resp.json();
+    res.send(investments);
+  } catch (e) {
+    console.error(inspect(e, false, null, true));
+    res.sendStatus(500);
+  }
+});
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.error("Error occurred starting the server", err)
-    process.exit(1)
+    console.error("Error occurred starting the server", err);
+    process.exit(1);
   }
-  console.log(`Server running on port ${config.port}`)
-})
+  console.log(`Server running on port ${config.port}`);
+});
